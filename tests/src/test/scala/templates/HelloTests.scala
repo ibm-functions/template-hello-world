10d35bdabb7c5e1369330945474127ce67b59e2c
Belinda Vennam <beemarie88@gmail.com>
2018-04-27 15:43:39 -0500

    update hello tests with spray json import (#16)

--- tests/src/test/scala/templates/HelloTests.scala
+++ tests/src/test/scala/templates/HelloTests.scala
@@ -26,7 +26,6 @@ import common.{TestHelpers, Wsk, WskProps, WskTestHelpers}
 import java.io._
 import com.jayway.restassured.RestAssured
 import com.jayway.restassured.config.SSLConfig
-import spray.json.DefaultJsonProtocol.StringJsonFormat
 import spray.json._
 import spray.json.DefaultJsonProtocol._
 
